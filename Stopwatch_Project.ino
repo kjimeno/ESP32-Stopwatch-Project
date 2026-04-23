7ebb9367bb622ff872689d6799d40d79cf937a43
Ken Jimeno <kbjimeno@gmail.com>
2026-04-22 20:28:09 -0500

    Set Time Menu Functionality Working

--- Stopwatch_Project.ino
+++ Stopwatch_Project.ino
@@ -12,9 +12,15 @@ const int POTENT_PIN = 35;
 int barCounter = 0;
 int countdown = 50;  // countdown in seconds
 float setTime = countdown;
+int setMinutes = countdown / 60;
+int setSeconds = countdown % 60;
 
-unsigned long lastUpdate = 0;
+unsigned long lastTimerUpdate = 0;
+unsigned long previousTime = 0;
 const int INTERVAL = 1000; // 1 second
+
+unsigned long setTimeCounter = 0;
+bool bLastTimerVisibility = true;
 
 enum MenuState {
   MAIN_MENU,
@@ -58,12 +64,15 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < idleFC; i++) {
+    float deltaTime = (millis() - previousTime);
+    previousTime = millis();
+
     bool bButtonPressed = digitalRead(BUTTON_PIN);
     int potentReading = analogRead(POTENT_PIN);
 
     switch (currentMenu) {
       case MAIN_MENU:
-        displayTimer();
+        displayTimer(deltaTime);
 
         //If potentiometer reading is turned to the left side,
         if (potentReading > 2048) {
@@ -94,6 +103,15 @@ void loop() {
           if (bButtonPressed) {
             //Display "press down" for right button
             tft.pushImage(148, 190, 102, 26, guiSetTimeButtons[2]);
+
+            //debounce for press animation
+            delay(1000);
+            //Hide main menu buttons and change to "set time" menu
+            tft.pushImage(0,0,280,240, background);
+            currentMenu = SET_TIME_MENU;
+            
+            break;
+
           } else {
             //Highlight the right button
             tft.pushImage(148, 190, 102, 26, guiSetTimeButtons[1]);
@@ -103,13 +121,40 @@ void loop() {
         }
         break;
 
-      case SET_TIME_MENU:
-        // show settings
+      case SET_TIME_MENU: {
+        Serial.println(buttonIdx);
+        if (bButtonPressed && buttonIdx < 2) {
+          buttonIdx++;
+          
+          //debounce
+          delay(1000);
+        } 
+        
+        if (buttonIdx >= 2) {
+          //Go back to main menu ----------------------------------
+          buttonIdx = 0;
+          countdown = setMinutes * 60 + setSeconds;
+          setTime = countdown;
+          tft.setTextColor(TFT_WHITE);
+
+          //debounce
+          delay(1000);
+          currentMenu = MAIN_MENU;
+
+          return;
+        }
+
+        if (buttonIdx == 0) {
+          setMinutes = 60 - analogRead(POTENT_PIN) * 60 / 4095;
+        } else if (buttonIdx == 1) {
+          setSeconds = 60 - analogRead(POTENT_PIN) * 60 / 4095;
+        }
+      
+        displayTimer(deltaTime);
 
         break;
-
+      }
       case PLAY_MENU:
-        // show sensor data
         updateTimer();
 
         break;
@@ -120,19 +165,90 @@ void loop() {
   }
 }
 
-void displayTimer() {
-  //background
-  tft.pushImage(0,35,280, 40, &background[280 * 35]);
-  
+void displayTimer(float deltaTime) {
+  bool bTimerVisibility = true;
+
   // convert to minutes and seconds
-  int minutes = countdown / 60;
-  int seconds = countdown % 60;
+  int minutes = setMinutes;
+  int seconds = setSeconds;
+
+  switch (currentMenu) {
+    case MAIN_MENU:
+      tft.pushImage(0,35,280, 40, &background[280 * 35]);
+      break;
       
+    case SET_TIME_MENU: {
+      setTimeCounter += deltaTime;
+      int switchVisibilityCount = setTimeCounter * 0.001;
+
+      //switch between visible and not visible on either minutes text or seconds
+      bTimerVisibility = switchVisibilityCount % 2 ? false : true;
+
+      tft.pushImage(0,35,280, 40, &background[280 * 35]);
+
+      if (bTimerVisibility) {
+        String timerText = "";
+        if (minutes < 10)
+          timerText += "0";
+        timerText += String(minutes) + ":";
+        if (seconds < 10)
+          timerText += "0";
+        timerText += String(seconds);
+        
+        tft.drawString(timerText, 140, 55);
+
+      } else {
+        String minText = "";
+        if (minutes < 10)
+          minText += "0";
+        minText += String(minutes);
+        
+        String secText = "";
+        if (seconds < 10)
+          secText += "0";
+        secText += String(seconds);
+
+        if (buttonIdx == 0) {
+          tft.setTextColor(TFT_DARKGREY);
+          tft.drawString(minText + ":" + secText, 140, 55);
+          tft.setTextColor(TFT_WHITE);
+          tft.drawString("  :" + secText, 140, 55);
+        } else if (buttonIdx == 1) {
+          tft.setTextColor(TFT_DARKGREY);
+          tft.drawString("  :" + secText, 140, 55);
+          tft.setTextColor(TFT_WHITE);
+          tft.drawString(minText + ":  ", 140, 55);
+        }
+
+        /*
+        if (buttonIdx == 0 && seconds < 10 && minutes < 10) {
+          tft.setTextColor(TFT_DARKGREY);
+          tft.drawString("0" + String(minutes) + ":0" + String(seconds), 140, 55);
+          tft.setTextColor(TFT_WHITE);
+          tft.drawString("  " + ":0" + String(seconds), 140, 55);
+        } else if (buttonIdx == 0 && seconds < 10) {
+          tft.setTextColor(TFT_DARKGREY);
+          tft.drawString(String(minutes) + ":0" + String(seconds), 140, 55);
+          tft.setTextColor(TFT_WHITE);
+          tft.drawString("  " + ":0" + String(seconds), 140, 55);
+        }
+        */
+      }
+      return;
+    }
+    case PLAY_MENU:
+      tft.pushImage(0,35,280, 40, &background[280 * 35]);
+      break;
+  }
+  
+
   // Draw centered at bottom
   if (seconds < 10)
     tft.drawString(String(minutes) + ":0" + String(seconds), 140, 55);  
   else
-    tft.drawString(String(minutes) + ":" + String(seconds), 140, 55);    
+    tft.drawString(String(minutes) + ":" + String(seconds), 140, 55);   
+
+  bLastTimerVisibility = bTimerVisibility;
 }
 
 void updateTimer() {
@@ -154,9 +270,9 @@ void updateTimer() {
   }
 
   // Update timer
-  if (millis() - lastUpdate >= INTERVAL && countdown > 0) {
-    lastUpdate = millis();
+  if (millis() - lastTimerUpdate >= INTERVAL && countdown > 0) {
+    lastTimerUpdate = millis();
     countdown--;
-    displayTimer();
+    displayTimer(0);
   }
 }
